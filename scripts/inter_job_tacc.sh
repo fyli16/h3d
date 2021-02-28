b7888ad219fa414140302fb2d25971b9aa0bfc80
Feiyu Li <fyli@login3.frontera.tacc.utexas.edu>
2021-02-28 00:58:23 -0600

    inter_job_tacc

--- scripts/inter_job_tacc.sh
+++ scripts/inter_job_tacc.sh
@@ -1,5 +1,4 @@
 #!/bin/tcsh
-
 module purge
 module load gcc/9.1.0 impi/19.0.9
 
@@ -8,11 +7,8 @@ setenv OMP_NUM_THREADS 1
 
 setenv DATA_DIRECTORY ./data
 setenv RESTART_DIRECTORY ./restart
-setenv INPUT_DIRECTORY ./
-setenv SOURCE_DIRECTORY ./
 mkdir -p $DATA_DIRECTORY
 mkdir -p $RESTART_DIRECTORY
-cp input.f90 $DATA_DIRECTORY/
 
-ibrun -np 56 $SOURCE_DIRECTORY/3dh 
-echo 'Done'
+ibrun -np 56 ./h3d
+exit
