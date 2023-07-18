9b54046dad4c0c55419b6f6b6aaad499ba905640
Feiyu Li <fyli.acad@gmail.com>
2023-07-18 14:20:02 -0600

    disable ez diag output

--- src/io.f90
+++ src/io.f90
@@ -57,8 +57,8 @@ module m_io
       ! open(unit=103,file=trim(data_directory)//filename3,status='unknown')
       ! write(filename3,"(a,i4.4,a)") 'ecal/ecal_ez_pass4_', myid, '.dat' 
       ! open(unit=104,file=trim(data_directory)//filename3,status='unknown')
-      write(filename3,"(a,i4.4,a)") 'ecal/ecal_ez_pass5_', myid, '.dat' 
-      open(unit=100,file=trim(data_directory)//filename3,status='unknown')
+      ! write(filename3,"(a,i4.4,a)") 'ecal/ecal_ez_pass5_', myid, '.dat' 
+      ! open(unit=100,file=trim(data_directory)//filename3,status='unknown')
     endif
 
   end subroutine open_hist_files
